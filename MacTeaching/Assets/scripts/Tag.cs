fe13e6a1f6dcb0954769d7485451536120787ce6
Wagner Queiroz <wagner.coder@gmail.com>
2018-05-30 11:42:35 -0300

    Basic Menu template added

---
+++ MacTeaching/Assets/scripts/Tag.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Tag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler {
+
+	private Image image;
+	private Vector3 position;
+
+	private void Awake()
+	{
+		image = GetComponent<Image>();
+		position = transform.position;
+	}
+
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		image.color = Color.green;
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		transform.position = eventData.position;
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		image.color = Color.white;
+		transform.position = this.position;
+	}
+}
