6a4d38a6939b7d8d87ac47859717d981e4a0b906
Wagner Queiroz <wagner.coder@gmail.com>
2018-06-01 00:38:35 -0300

    Add class DataHandle && Update Basic Menu

---
+++ MacTeaching/Assets/scripts/DataHandle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataHandle : MonoBehaviour {
+
+	string URL = "";
+
+	void Start(){
+		getDataTest();
+	}
+
+	// Use this for initialization
+	public void getDataTest () {
+		StartCoroutine(getData("wagner"));
+	}
+
+	IEnumerator getData (string data){
+		this.URL = this.URL + 23;
+		WWW getDelivery = new WWW(this.URL);
+
+		yield return getDelivery;
+		Debug.Log(getDelivery.text);
+	}
+}
